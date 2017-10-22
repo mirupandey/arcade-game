ab738eae218a6598c9dda788f2020ad3e0bcf982
Mrinal Pandey <mrinalpandey6@gmail.com>
2017-10-22 20:51:21 +0530

    new
    new

---
+++ README.md
@@ -0,0 +1,9 @@
+# Classic Arcade Game
+
+## How to Play
+
+> The user can control the movement of the player using arrow keys.
+
+> The player must not collide with the enemy/enemies. If it does the position of the player gors back to the beginning.
+
+> As soon as the player reaches the water the player wins and resets to the original position.
---
+++ js/app.js
@@ -0,0 +1,103 @@
+// Draw the enemy and player objects on the screen
+Object.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+//Reset player to beginning position
+Object.prototype.reset = function() {
+  player.x = 200;
+  player.y = 400;
+}
+
+/*
+    Enemy Objects
+*/
+
+// Enemies the player must avoid
+var Enemy = function(x,y) {
+
+    // The image/sprite for enemies
+    this.sprite = 'images/enemy-bug.png';
+
+    //x and y coordinates and movement speed
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 200) + 100);
+}
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    if(this.x <= 550){
+        this.x += this.speed * dt;
+    }else{
+        this.x = -2;
+    }
+
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+            this.reset();
+        }
+    }
+}
+
+/*
+    Player Object
+*/
+
+// Player class and initial x and y coordinates
+var Player = function(){
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+}
+
+//Update player position
+Player.prototype.update = function(){
+    if(this.controtlPosition === 'left' && this.x > 0){ 
+        this.x = this.x - 50; 
+    }else if(this.controtlPosition === 'right' && this.x != 400){
+        this.x = this.x + 50;
+    }else if(this.controtlPosition === 'up'){
+        this.y = this.y - 50;
+    }else if (this.controtlPosition === 'down' && this.y != 400){
+        this.y = this.y + 50;
+    }
+    this.controtlPosition = null;
+    
+    if(this.y < 25){
+        this.reset();
+    }
+}
+
+
+//Input handler for player
+Player.prototype.handleInput = function(e){
+    this.controtlPosition = e;    
+}
+
+
+// Instantiate enemies and player objects
+var allEnemies = [];
+(function setEnemies(){
+    allEnemies.push(new Enemy(-2, 60));
+    allEnemies.push(new Enemy(-2, 100));
+    allEnemies.push(new Enemy(-2,150));
+    allEnemies.push(new Enemy(-2,220));
+}());
+
+var player = new Player(); 
+
+
+// listens for key presses and sends the keys to 
+// Player.handleInput() method.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
